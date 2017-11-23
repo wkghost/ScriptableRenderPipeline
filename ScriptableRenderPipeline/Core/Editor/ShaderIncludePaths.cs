c8c31eb8bc38e232739a1e928ffc7b03967ea91b
Tim Cooper <tim@unity3d.com>
2017-11-23 08:20:42 +0100

    temp commit

---
+++ ScriptableRenderPipeline/Core/Editor/ShaderIncludePaths.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class ShaderIncludePaths : ScriptableObject, IShaderIncludePathGenerator
+{
+    [SerializeField]
+    List<string> m_Paths = new List<string>();
+
+    public void AddPaths(IEnumerable<string> paths)
+    {
+        m_Paths.AddRange(paths);
+    }
+
+    public void GetShaderIncludePaths(List<string> paths)
+    {
+        foreach (var path in m_Paths)
+            paths.Add(Path.GetFullPath(path));
+    }
+}
---
+++ ScriptableRenderPipeline/Core/Editor/ShaderIncludePathsImporter.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+[ScriptedImporter(1, kExtension)]
+public class ShaderIncludePathsImporter : ScriptedImporter
+{
+    public const string kExtension = "ShaderIncludePaths";
+	public override void OnImportAsset(AssetImportContext ctx)
+	{
+		var pathStrings = File.ReadAllLines(ctx.assetPath);
+		var paths = ScriptableObject.CreateInstance<ShaderIncludePaths>();
+		paths.hideFlags |= HideFlags.NotEditable;
+		paths.AddPaths(pathStrings);
+		ctx.AddObjectToAsset("MainAsset", paths);
+		ctx.SetMainObject(paths);
+	}
+}
+
+class ShaderIncludePostProcess : AssetPostprocessor
+{
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        if (importedAssets.Any(x => x.EndsWith(ShaderIncludePathsImporter.kExtension, true, CultureInfo.InvariantCulture))
+            || deletedAssets.Any(x => x.EndsWith(ShaderIncludePathsImporter.kExtension, true, CultureInfo.InvariantCulture))
+            || movedAssets.Any(x => x.EndsWith(ShaderIncludePathsImporter.kExtension, true, CultureInfo.InvariantCulture)))
+        {
+            ShaderUtil.UpdateShaderIncludePaths();
+
+            AssetDatabase.StartAssetEditing();
+            string[] allAssetPaths = AssetDatabase.GetAllAssetPaths();
+            foreach (string assetPath in allAssetPaths)
+            {
+                var shader = AssetDatabase.LoadAssetAtPath(assetPath, typeof(Shader)) as Shader;
+                if (shader != null)
+                    AssetDatabase.ImportAsset(assetPath);
+            }
+            AssetDatabase.StopAssetEditing();
+        }
+    }
+}
